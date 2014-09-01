badf9b7e235552616314c72e8ea27849978de6f0
Anthony Edwards Jr <credential.helper>
2014-09-01 00:02:32 -0400

    not working change

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -1,4 +1,5 @@
 require 'dragonfly'
+# require 'dragonfly/s3_data_store'
 
 # Configure
 Dragonfly.app.configure do
@@ -8,9 +9,12 @@ Dragonfly.app.configure do
 
   url_format "/media/:job/:name"
 
-  datastore :file,
-    root_path: Rails.root.join('public/system/dragonfly', Rails.env),
-    server_root: Rails.root.join('public')
+  # if Rails.env.development? || Rails.env.test?
+  datastore :file,
+            root_path: Rails.root.join('public/system/dragonfly', Rails.env),
+            server_root: Rails.root.join('public')
+  # end
+
 end
 
 # Logger
