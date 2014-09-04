2dfe99bb45cb5584f3126e23eec446dc0686bdd9
Anthony Edwards Jr <credential.helper>
2014-09-04 14:10:18 -0400

    added javascript to make images appear or dissappear for category

--- app/assets/javascripts/photos.js
+++ app/assets/javascripts/photos.js
@@ -1,4 +1,33 @@
 document.addEventListener('DOMContentLoaded', function(){
-	console.log("content loaded")
+	console.log("content loaded");
+
+	var categories = document.querySelector(".categories")
+	categories.addEventListener('click', showcategory)
 
 });
+
+function showcategory(event){
+	//show individual category or show all
+	var category = event.target.innerHTML;
+
+	if (category !== "all"){			
+		var photos = document.querySelectorAll('li[data-category]');
+		 // console.log(photos.dataset.data-category=== category);
+		for(var i = 0; i<photos.length; i++){
+			 console.log(photos[i].dataset.category=== category)
+			if (photos[i].dataset.category !== category){
+			photos[i].style.display = "none";
+			}
+		}
+	}else{ 
+		//make images visible
+		var photos = document.querySelectorAll('li[data-category]');
+		console.log(photos)
+		for(var i = 0; i<photos.length; i++){
+			photos[i].style.display = "";
+		}
+	}//end if
+	
+
+	
+}//end showcategory
