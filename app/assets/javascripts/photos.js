a5263d629a9b1e081b02ed73bf31f3e53641843c
Anthony Edwards Jr <credential.helper>
2014-09-05 14:09:32 -0400

    added shuffle algorithm to array and wired the shuffle link

--- app/assets/javascripts/photos.js
+++ app/assets/javascripts/photos.js
@@ -9,8 +9,9 @@ document.addEventListener('DOMContentLoaded', function(){
 function showcategory(event){
 	//show individual category or show all
 	var category = event.target.innerHTML;
+	console.log(category)
 
-	if (category !== "all"){			
+	if (category !== "all" && category != "shuffle"){			
 		var photos = document.querySelectorAll('li[data-category]');
 		 // console.log(photos.dataset.data-category=== category);
 		for(var i = 0; i<photos.length; i++){
@@ -19,7 +20,44 @@ function showcategory(event){
 			photos[i].style.display = "none";
 			}
 		}
-	}else{ 
+	}else if(category === "shuffle") {
+		console.log("inside heree");
+
+		// remove photos,
+		// add photos in diffeent order
+			Array.prototype.shuffle = function (){
+			    var i = this.length, j, temp;
+			    if ( i == 0 ) return;
+			    while ( --i ) {
+			        j = Math.floor( Math.random() * ( i + 1 ) );
+			        temp = this[i];
+			        this[i] = this[j];
+			        this[j] = temp;
+			    }
+            };
+
+            var turnObjToArray = function(obj) {
+			  return [].map.call(obj, function(element) {
+			    return element;
+			  })
+			};
+
+			var photos = document.querySelectorAll('li[data-category]');
+			var parent = document.querySelector("#photos-list");
+
+			var listimages = turnObjToArray(photos);
+			listimages.shuffle();
+
+			//remove elements from parent
+			while (parent.firstChild) {
+			  parent.removeChild(parent.firstChild);
+			}		
+
+			for(var i = 0; i<listimages.length; i++){
+			  parent.appendChild(listimages[i])
+			}
+
+	}else { 
 		//make images visible
 		var photos = document.querySelectorAll('li[data-category]');
 		console.log(photos)
