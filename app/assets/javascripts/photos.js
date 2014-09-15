d299f0a53d54e01fdebe0ee32c5b476034d312fc
Anthony Edwards Jr <credential.helper>
2014-09-15 10:20:03 -0400

    added cloudfront in production

--- app/assets/javascripts/photos.js
+++ app/assets/javascripts/photos.js
@@ -24,7 +24,6 @@ document.addEventListener('DOMContentLoaded', function(){
                 header.classList.remove("smaller");
             }
                // header.classList.toggle("smaller");
-
         }
     });
 
