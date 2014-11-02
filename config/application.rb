e97c4baeb50726f5d6c30be8e5459d68057cc8a4
Anthony Edwards Jr <credential.helper>
2014-11-02 10:50:39 -0600

    removed .env

--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-require 'dotenv'
-Dotenv.load
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
