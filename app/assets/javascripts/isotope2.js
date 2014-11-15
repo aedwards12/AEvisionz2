598713c457d74740d3a68a1983c333a8d0aa00a9
Anthony Edwards Jr <credential.helper>
2014-11-15 13:55:44 -0500

    changed onload

--- app/assets/javascripts/isotope2.js
+++ app/assets/javascripts/isotope2.js
@@ -1,20 +1,37 @@
 $(window).load(function(){
 
- $container = $('#masonry-container');
+ // $container = $('#masonry-container');
 
  $(window).width() < 600 ? $( ".box" ).removeClass( "col2" ).addClass( "col3" )  : 10;
 
-    $container.imagesLoaded( function(){
-      $container.isotope({
+ var $container = $('#masonry-container').isotope({
+    
         itemSelector: '.box',
     
         masonry: {
           columnWidth: 5,
           isFitWidth: true
         }
+      });
 
-      }); 
-  });
+
+ $container.imagesLoaded(function(){
+  $container.isotope('layout');
+ })
+
+  //   $container.imagesLoaded()
+   
+  //   .done( function( instance ){
+  //     $container.isotope({
+  //       itemSelector: '.box',
+    
+  //       masonry: {
+  //         columnWidth: 5,
+  //         isFitWidth: true
+  //       }
+
+  //     }); 
+  // });
   
   $('#nav > li > ul').on( 'click', 'li', function() {
     var filterValue = $(this).attr('data-filter');
